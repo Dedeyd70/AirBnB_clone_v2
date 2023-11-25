56d44e87934be307a8bd4e603e5d90e65a23fb8b
Dedeyd70 <dorothyokoto@gmail.com>
2023-11-24 19:33:43 -0800

    it is a number

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+""" A script that starts a Flask web application"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """Display 'Hello HBNB!' on the main route"""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """displays 'HBNB' on the /hbnb route."""
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_text(text):
+    """Display 'C' follopwed by the value of tst variable."""
+    txt = text.replace("_", " ")
+    return "C {}".format(txt)
+
+
+@app.route("/python", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def python(text='is cool'):
+    """Print python followed by the value of the text variable"""
+    txt = text.replace("_", " ")
+    return "Python {}".format(txt)
+
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def number(n):
+    """Displaying n is a number, only if is an integer"""
+    return "{} is a number".format(n)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
